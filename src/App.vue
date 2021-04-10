832916694d248cd2a63cf1802c1e6f30066407cb
yuan_wy_wang <631824375@qq.com>
2021-04-10 18:33:17 +0800

    咋没用呢

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      123123
+  11111
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
